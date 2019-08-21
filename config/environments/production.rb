57aa64890368c2a0b1d85be878a0afd5a98df761
toniahurst <antonia.a.hurst@gmail.com>
2019-08-21 19:50:23 +0000

    L11. Added devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'flixxer-tonia-hurst.herokuapp.com' }
+# Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
