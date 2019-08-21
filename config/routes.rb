7db6521404162da8224c0a2411ec6c2fe0f689cb
toniahurst <antonia.a.hurst@gmail.com>
2019-08-21 22:39:17 +0000

    L21. Added course and lesson details.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
-  	resources :sections, only: [] do
-  		resources :lessons, only: [:new, :create] 
-  	end
-  		resources :courses, only: [:new, :create, :show] do
-  			resources :sections, only: [:new, :create]
-  	end
-	end
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
+  end
 end
