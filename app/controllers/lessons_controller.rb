738bbf0e574e03863a789ec7070b8092d92cec42
toniahurst <antonia.a.hurst@gmail.com>
2019-08-22 04:18:47 +0000

    L30. Ordered lessons. Fixing issues.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,17 +1,11 @@
 class LessonsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course, only: [:show]
+	
 
 	def show
 	end
 
 	  private
-
-	  def require_authorized_for_current_lesson
-  	if !current_user.enrolled_in?(current_lesson.section.course)
-  		redirect_to course_path(current_lesson.section.course), alert: 'You must enroll first'
-  	end
-  end
 
   helper_method :current_lesson
   def current_lesson
